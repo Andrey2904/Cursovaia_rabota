574cb000c138c01cfb3b8e4b47fa8dc006d944d3
Andrey2904 <svyazov.andrei@yandex.ru>
2023-04-24 21:34:00 +0300

    Test

---
+++ kursac_2.cpp
@@ -0,0 +1,197 @@
+﻿// kursac_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+void show(vector<int>a) {
+    for (int i = 0; i < a.size(); i++) {
+        cout << a[i] << " ";
+    }
+}
+int partition(vector<int>& arr, int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+void quickSort(vector<int>& arr, int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+
+int main()
+{   
+    int fbite, ebite;
+    string path;
+    cout << "Enter path" << endl;
+    cin >> path;
+    fstream file(path,fstream::in|fstream::out ,ios_base::binary);
+    long nFileLen = 0;
+    int size_user;
+    cout << "Enter size of fail"<<endl;
+    cin >> size_user;
+    if (file)
+    {
+        file.seekg(0, ios_base::end);
+        nFileLen = file.tellg();
+        file.close();
+    }
+    else {
+        cout << "Autosize is not define"<<endl;
+        nFileLen = -1;
+    }
+    cout << "Autosize is " << nFileLen << endl;
+    cout << "Enter first bite and last bite" << endl;
+    cin >> fbite>>ebite;
+    file.open(path);
+    string start,midle,ends;
+    int pos = 0;
+    if (size_user != nFileLen) {
+        cout << "The file size determined by the program does not match the one entered by the user."<<endl;
+        cout << "The file size will be determined by the program" << endl;
+    }
+    if (!file.is_open()) {
+        cout << "Error. File is not open" << endl;
+        return -1;
+    }
+    else {
+        
+        char ch;
+        
+        while (file.get(ch)) {
+            
+            if (pos == fbite) {
+                break;
+            }
+            start += ch;
+            pos++;
+
+        }
+        cout << endl;
+        while (file.get(ch)) {
+            midle += ch;
+            if (pos == ebite) {
+                break;
+            }
+            
+            pos++;
+        }
+        cout << endl;
+        while (file.get(ch)) {
+            if (pos == nFileLen)
+                break;
+            ends += ch;
+            pos++;
+        }
+    }
+    vector<char> mas;
+    pos = 0;
+    file.close();
+    for (int i = 0; i < midle.size(); i++) {
+        mas.push_back(midle[i]);
+    }
+    vector<int> num;
+    for (int i = 0; i < mas.size(); i++) {
+        int tm = int(mas[i]);
+        num.push_back(tm);
+
+    }
+
+    show(num);
+    vector<int> id,ent;
+    cout << endl;
+    for (int i = 0; i < num.size(); i++) {
+        if (num[i] == 32) {
+            id.push_back(i);
+        }
+        if (num[i] == 10) {
+            ent.push_back(i);
+        }
+    }
+   
+    auto result = remove(num.begin(), num.end(), 32);
+    num.erase(result, num.end());
+    result = remove(num.begin(), num.end(), 10);
+    num.erase(result, num.end());
+
+    int number_of_elem = num.size();
+    quickSort(num, 0, number_of_elem - 1);
+    cout << endl;
+
+    
+    for (int i = 0; i < id.size(); i++) {
+        auto it = num.begin()+id[i];
+        num.insert(it, 32);
+    }
+    for (int i = 0; i < ent.size(); i++) {
+        auto it = num.begin() + ent[i];
+        num.insert(it, 10);
+    }
+
+ 
+
+    midle = "";
+    char a;
+    for (int i = 0; i < num.size(); i++) {
+        a = num[i];
+        midle += a;
+        
+    }
+
+
+    path = "C:\\Users\\HP\\Desktop\\prog\\myfile.txt";
+    ofstream newfile;
+    
+    
+    newfile.open(path);
+
+    if (!newfile.is_open()) {
+        cout << "Error, file is not open";
+    }
+    else {
+        newfile << start;
+        newfile << midle;
+        newfile << ends;
+    }
+    newfile.close();
+
+    path = "C:\\Users\\HP\\Desktop\\prog\\myfile_srt.SRT";
+
+
+    newfile.open(path);
+
+    if (!newfile.is_open()) {
+        cout << "Error, file is not open";
+    }
+    else {
+        newfile << start;
+        newfile << midle;
+        newfile << ends;
+    }
+    newfile.close();
+    
+}
